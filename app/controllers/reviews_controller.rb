b898f3af9893e9559d628d4b77b1e86b35b19a8b
github username <rvtingling@yahoo.com>
2021-04-03 21:55:38 -0400

    updated reviews controller with other actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,4 +6,36 @@ class ReviewsController < ApplicationController
         render json: reviews
         # render json: quoteTypes, include: [:freight_quotes]
     end     
+
+
+    def create 
+        review = Review.new(review_params)
+        if review.save 
+            render json: review
+            # render json: FreightQuoteSerializer.new(review)
+        else 
+            render json: {errors: review.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    def destroy 
+        review = Review.find_by_id(params[:id])
+        if review.destroy
+            reviews = Review.all 
+            render json: reviews
+            # render json: FreightQuoteSerializer.new(reviews)
+        else
+            render json: { message: "error"}  
+        end 
+    end 
+
+    private
+
+    def review_params
+        params.require(:review).permit(:comment, :reservation_id, :client_id)
+    end 
+
+    def find_review
+        @review = Review.find_by_id(params[:id])
+    end
 end
