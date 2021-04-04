c1a9f68c0aecf01838235abcdfe3f32ea4e6aa40
github username <rvtingling@yahoo.com>
2021-04-03 21:49:14 -0400

    created index action for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,9 @@
 class ReviewsController < ApplicationController
 
 
-    
+    def index 
+        reviews = Review.all
+        render json: reviews
+        # render json: quoteTypes, include: [:freight_quotes]
+    end     
 end
