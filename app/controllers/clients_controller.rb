171f5b543cf46976fe45b2fbb67dbbdf46982564
github username <rvtingling@yahoo.com>
2021-04-03 21:37:41 -0400

    created private methods and destroy method

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -19,4 +19,25 @@ class ClientsController < ApplicationController
             render json: {errors: client.errors.full_messages}, status: :unprocessible_entity
         end 
     end 
+
+    def destroy 
+        client = Client.find_by_id(params[:id])
+        if client.destroy
+            clients = Client.all 
+            render json: clients
+            # render json: ShipperSerializer.new(clients)
+        else
+            render json: { message: "error"}  
+        end 
+    end 
+
+    private
+
+    def client_params
+        params.require(:client).permit(:first_name, :last_name, :email, :phone, :password)
+    end 
+
+    def find_client
+        @client = Client.find_by_id(params[:id])
+    end
 end
