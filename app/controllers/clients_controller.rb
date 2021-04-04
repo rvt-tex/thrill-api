43040752f18024ca432941bbfff2a1b4348027ff
github username <rvtingling@yahoo.com>
2021-04-03 21:33:08 -0400

    created show and create actions

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,4 +4,19 @@ class ClientsController < ApplicationController
         clients = Client.all 
         render json: clients
     end 
+
+    def show 
+        @client = Client.find_by_id(params[:id])
+        render json: @client
+    end 
+    
+    def create 
+        client = Client.new(client_params)
+        if client.save 
+            render json: client
+            # render json: ShipperSerializer.new(shipper)
+        else 
+            render json: {errors: client.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
 end
