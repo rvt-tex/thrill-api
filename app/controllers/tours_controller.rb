feba2a8918f43a8a6425509e463055cbf5d591dd
github username <rvtingling@yahoo.com>
2021-04-03 21:59:58 -0400

    fixed typo in tours controller

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,7 +1,7 @@
 class ToursController < ApplicationController
 
     def index 
-        tours = Tours.all
+        tours = Tour.all
         render json: tours
         # render json: quoteTypes, include: [:freight_quotes]
     end 
