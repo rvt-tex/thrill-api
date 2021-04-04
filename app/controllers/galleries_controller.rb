e86987d92ad234caf0c47dda6c9ee5c45c3c5b0a
github username <rvtingling@yahoo.com>
2021-04-03 21:48:14 -0400

    added index action

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,2 +1,8 @@
 class GalleriesController < ApplicationController
+
+    def index 
+        galleries = Gallery.all
+        render json: galleries
+        # render json: quoteTypes, include: [:freight_quotes]
+    end 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
 
+
     
 end
