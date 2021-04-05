8a2b65ed45c58c03a73a61e23624e146dbd00d36
github username <rvtingling@yahoo.com>
2021-04-05 16:43:11 -0400

    updated controller action

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -5,7 +5,7 @@ class AuthController < ApplicationController
         client = Client.find_by(email: params[:email])
         if client && client.authenticate(params[:password])
             token = encode_token({client_id: client.id})
-            render json: { jwt: token}
+            render json: { jwt: token, client: client}
         else 
             render json: {error: "Incorrect email or password"}, status: :unauthorized 
         end 
