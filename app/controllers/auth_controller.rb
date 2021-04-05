14ca19091e42f7625a2f7e68ae5f8106a76e9b84
github username <rvtingling@yahoo.com>
2021-04-04 20:54:53 -0400

    created a create action in auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,12 @@
 class AuthController < ApplicationController
+
+    def create 
+        client = Client.find_by(email: params[:email])
+        if client && client.authenticate(params[:password])
+            token = encode_token({client_id: client.id})
+            render json: token
+        else 
+            render json: {error: "Incorrect email or password"}, status: :unauthorized 
+        end 
+    end 
 end
